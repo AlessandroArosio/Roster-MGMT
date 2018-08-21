eceee0ed173ee9f008c6d8e8f911bc7c81d63407
Alessandro Arosio <alessandro.arosio1@gmail.com>
2018-08-21 23:16:43 +0100

    Working on displaying multiple rota for the same branch
    TODO:
    Mat-card-header should display rotas per week
    Add pagination

--- src/app/rota/rota-list/rota-list.component.ts
+++ src/app/rota/rota-list/rota-list.component.ts
@@ -11,50 +11,101 @@ import {UsersService} from '../../users/users.service';
   styleUrls: ['./rota-list.component.css']
 })
 export class RotaListComponent implements OnInit, OnDestroy {
-rotas: Rota[] = [];
-users: User[] = [];
-isLoading = false;
-headerArray = [];
-displayedColumns: string[] = ['employeeName', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-private rotasSub: Subscription;
-private usersSub: Subscription;
+  rotas: Rota[] = [];
+  users: User[] = [];
+  isLoading = false;
+  headerArray = [];
+  testArray = [];
+  boh = [];
+  // boh: [{
+  //   branch: string,
+  //   weeklyRota: [{
+  //     startDate: string,
+  //     employeeName: string[],
+  //     shifts: string[]
+  //   }]
+  // }];
+  private rotasSub: Subscription;
+  private usersSub: Subscription;
 
-constructor(public rotaService: RotaService, public usersService: UsersService) {}
+  constructor(public rotaService: RotaService, public usersService: UsersService) {}
 
-ngOnInit() {
-  this.isLoading = true;
-  this.rotaService.getRotas();
-  this.usersService.getUsers();
-  this.rotasSub = this.rotaService
-    .getRotaUpdateListener()
-    .subscribe((rotas: Rota[]) => {
-      this.isLoading = false;
-      this.rotas = rotas;
-      console.log(rotas);
-      console.log(rotas[1].branchName.toString());
-      for (let i = 0; i < rotas.length; i++) {
-        this.headerArray.push(rotas[i].branchName);
+  ngOnInit() {
+    this.isLoading = true;
+    this.rotaService.getRotas();
+    this.usersService.getUsers();
+    this.rotasSub = this.rotaService
+      .getRotaUpdateListener()
+      .subscribe((rotas: Rota[]) => {
+        this.isLoading = false;
+        this.rotas = rotas;
+        let found = false;
+        for (let i = 0; i < rotas.length; i++) {
+          for (let j = 0; j < this.testArray.length; j++) {
+            if (rotas[i].branchName === rotas[j].branchName) {
+              this.testArray[j].manyRotasStart.push(rotas[i].rotaStartDate);
+              const obj = {
+                startDate: rotas[i].rotaStartDate + ' <---> ' + rotas[i].rotaEndDate,
+                employeeName: rotas[i].employeeName,
+                shifts: rotas[i].shifts
+              };
+              this.boh[j].weeklyRota.push(obj);
+
+
+              this.testArray[j].manyRotasEnd.push(rotas[i].rotaEndDate);
+              rotas[i].employeeName.forEach((user) => {
+                this.testArray[j].employeeName.push(user);
+              });
+              rotas[i].shifts.forEach((shift) => {
+                this.testArray[j].shifts.push(shift);
+              });
+              found = true;
+            }
+          }
+          if (!found) {
+            this.headerArray.push(rotas[i].branchName);
+            this.boh.push({
+              branch: rotas[i].branchName,
+              weeklyRota: [{
+                startDate: rotas[i].rotaStartDate + ' <---> ' + rotas[i].rotaEndDate,
+                employeeName: rotas[i].employeeName,
+                shifts: rotas[i].shifts
+              }]
+            });
+
+            this.testArray.push({
+              branch: rotas[i].branchName,
+              manyRotasStart: [rotas[i].rotaStartDate],
+              manyRotasEnd: [rotas[i].rotaEndDate],
+              shifts: rotas[i].shifts,
+              employeeName: rotas[i].employeeName
+            });
+            found = false;
+          }
+          found = false;
+        }
+        console.log(this.testArray);
+        console.log(rotas);
+        console.log(this.boh);
+      });
+    this.usersSub = this.usersService
+      .getUserUpdateListener()
+      .subscribe((users: User[]) => {
+        this.users = users;
+      });
+  }
+
+  findUserName(id: string) {
+    for (let i = 0; i < this.users.length; i++) {
+      if (id === this.users[i].id) {
+        return this.users[i].firstName + ' ' + this.users[i].lastName;
       }
-      console.log(this.headerArray.toString());
-    });
-  this.usersSub = this.usersService
-    .getUserUpdateListener()
-    .subscribe((users: User[]) => {
-      this.users = users;
-    });
-}
-
-findUserName(id: string) {
-  for (let i = 0; i < this.users.length; i++) {
-    if (id === this.users[i].id) {
-      return this.users[i].firstName + ' ' + this.users[i].lastName;
     }
   }
-}
 
 
-ngOnDestroy() {
-  this.rotasSub.unsubscribe();
-}
+  ngOnDestroy() {
+    this.rotasSub.unsubscribe();
+  }
 
 }
