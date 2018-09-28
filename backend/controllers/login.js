0b4b50a873e7925dd161b3afef7ed89b381c54d3
Alessandro Arosio <alessandro.arosio1@gmail.com>
2018-09-28 13:13:52 +0100

    LoginController completed
    TODO:
    2. Prettify some forms and login (CSS only)
    3. General refactoring and code polishing.
    4. Deployment
    
    ~~~~ END ~~~~

---
+++ backend/controllers/login.js
@@ -0,0 +1,38 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.userLogin = (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed 1"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Invalid authentication credentials!"
+        });
+      }
+      const token = jwt.sign(
+        {email: fetchedUser.email, userId: fetchedUser._id},
+        'secret_word_for_jsonwebtoken_roster-mgmt',
+        {expiresIn: '1h'}
+      );
+      res.status(200).json({
+        token: token,
+        expiresIn: 3600
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed 3"
+      });
+    });
+}
--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,42 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const LoginController = require('../controllers/login');
 
-router.post('', (req, res, next) => {
-  let fetchedUser;
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({
-          message: "Auth failed 1"
-        });
-      }
-      fetchedUser = user;
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then(result => {
-      if (!result) {
-        return res.status(401).json({
-          message: "Invalid authentication credentials!"
-        });
-      }
-      const token = jwt.sign(
-        {email: fetchedUser.email, userId: fetchedUser._id},
-        'secret_word_for_jsonwebtoken_roster-mgmt',
-        {expiresIn: '1h'}
-        );
-      res.status(200).json({
-        token: token,
-        expiresIn: 3600
-      });
-    })
-    .catch(err => {
-      return res.status(401).json({
-        message: "Auth failed 3"
-      });
-    });
-});
+router.post('', LoginController.userLogin);
 
 module.exports = router;
